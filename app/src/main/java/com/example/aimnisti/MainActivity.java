4f83dbb52b91b53b5eb25c58d3c84b2a441dfe07
chungHee Han <chunghuihan9gmail.com>
2022-08-11 22:37:52 +0900

    first commit

---
+++ app/src/main/java/com/example/aimnisti/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.aimnisti;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+
+
+public class MainActivity extends AppCompatActivity {
+    ImageView imageView;
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        imageView = findViewById(R.id.imageview1);
+        button = findViewById(R.id.button1);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent, 1);
+            }
+        });
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch(requestCode) {
+            case 1:
+                if (resultCode == RESULT_OK) {
+                    Uri uri = data.getData();
+                    imageView.setImageURI(uri);
+                }
+                break;
+        }
+    }
+}
